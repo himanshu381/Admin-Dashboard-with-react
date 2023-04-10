d8909a56806e6143d5a2d39bae65b1521041e4a9
Himanshu Baranwal <84630522+himanshu381@users.noreply.github.com>
2023-04-10 15:27:10 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://thumbs.dreamstime.com/b/beautiful-fashion-model-asian-girl-modern-dress-26294085.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id:2,
+        img: "https://images-na.ssl-images-amazon.com/images/I/71xWrUr44XL._UL1500_.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/71VolZOyHwL._UL1500_.jpg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+];
+export const categories = [
+    {
+        id: 1,
+        img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/18829456/2022/7/4/7f27c8eb-2040-481c-aa1f-360257f6f9b41656934123938-Calvin-Klein-Jeans-Men-Shirts-4951656934123401-1.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id : 2,
+        img: "https://www.dmarge.com/wp-content/uploads/2016/01/tee2.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://assets.vogue.com/photos/6282b595b39e3a55ed8c9562/1:1/w_2667,h_2667,c_limit/slide_6.jpg",
+        title: "Light JACKETS",
+    },
+];
+    export const popularProducts = [
+        {
+            id: 1,
+            img: "https://m.media-amazon.com/images/I/61yBCMmuI4L._UL1440_.jpg",
+        },
+        {
+            id: 2,
+            img: "https://www.redwolf.in/image/catalog/mens-t-shirts/full-sleeves/batman-emblem-full-sleeve-t-shirt-india.jpg",
+    
+        },
+        {
+            id: 3,
+            img: "https://sc04.alicdn.com/kf/HTB1PNGYKeuSBuNjSsziq6zq8pXa9.jpg",
+        },
+        {
+            id: 4,
+            img: "https://media.gq.com/photos/5fe3508f9d8a32220e87a222/master/w_1280%2Cc_limit/Rockmount-denim-floral-western-embroidered-cowboy-shirt.jpg",
+        },
+        {
+            id: 5,
+            img: "https://media.gq-magazine.co.uk/photos/611d02fb6919346867797f5c/master/w_1920,h_1280,c_limit/JEANS_0000_M&S.jpg"
+        },
+        {
+            id: 6,
+            img: "https://media.vogue.co.uk/photos/6303873f26e3a8a3a5faa8e5/1:1/w_640,h_640,c_limit/PRODUCT.png"
+        },
+        {
+            id: 7,
+            img: "https://www.collinsdictionary.com/images/full/trousers_29362489_1000.jpg"
+        },
+        {
+            id: 8,
+            img: "https://previews.123rf.com/images/popovaphoto/popovaphoto1802/popovaphoto180200039/95079841-men-s-trousers-isolated-on-white-background.jpg"
+        },
+
+    ];
